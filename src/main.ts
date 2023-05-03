944f9abac3572041269f382ccfd7781cdd7f2684
eduek <eduekakpannyah@gmail.com>
2023-05-03 11:53:29 +0100

    feat: app functionality

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,58 @@
 import './styles/style.scss';
-import { setupCounter } from './counter.ts'
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
+/** adding a grade field to the form */
+const courseListEl = document.querySelector<HTMLDivElement>("[data-course-list]")!;
+const addGradeBtn = document.querySelector<HTMLButtonElement>("[data-btn-grade]")!;
 
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+addGradeBtn.addEventListener("click", () => {
+  const gradeEl = document.createElement('div');
+  gradeEl.classList.add('grade');
+  gradeEl.innerHTML = `
+    <div class="form-group form-group--one">
+      <h4 class="form-group__title">course:</h4>
+      <div class="wrap wrap--input">
+        <input type="text" name="courseTitle">
+      </div>
+    </div>
+
+    <div class="form-group form-group--two">
+      <h4 class="form-group__title">score:</h4>
+      <div class="wrap wrap--input">
+        <input type="number" min="0" max="100" name="courseScore">
+      </div>
+    </div>
+  `;
+  courseListEl.appendChild(gradeEl);
+});
+
+
+/** submit grade form  */
+const gradeFormEl = document.querySelector<HTMLFormElement>("[data-grade-form]")!;
+
+gradeFormEl.addEventListener("submit", (e: SubmitEvent) => {
+  e.preventDefault();
+});
+
+
+/** opening and closing the semester grades form */
+const semesterBtn = document.querySelector<HTMLButtonElement>("[data-btn-semester]")!;
+const closeModalBtn = document.querySelector<HTMLButtonElement>("[data-btn-close]")!;
+const mainContentEl = document.querySelector<HTMLDivElement>("[data-main]")!;
+const modalContentEl = document.querySelector<HTMLDivElement>("[data-modal]")!;
+
+  // toggle open/close classe on modal 
+function toggleVisible() {
+  mainContentEl.classList.toggle('main--blur');
+  modalContentEl.classList.toggle('modal--visible');
+};
+
+  // open modal
+semesterBtn.addEventListener("click", toggleVisible);
+
+  // close modal
+modalContentEl.addEventListener("click", (e: Event) => {
+  if (e.target === e.currentTarget) toggleVisible();
+});
+
+  // open/close modal with the close button
+closeModalBtn.addEventListener("click", toggleVisible);
